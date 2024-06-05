bd5d0a371048a6ad748c88c129c528612e664212
Gabriel <gabrielrr96@gmail.com>
2024-06-05 17:46:34 -0300

    None.

--- parallelization.py
+++ parallelization.py
@@ -24,13 +24,13 @@ if __name__ == "__main__":
     B_x = B * np.cos(theta)
     B_y = B * np.sin(theta)
     Lambda = 0.56 #5*Delta/k_F
-    Omega = 0.001 #0.02
+    Omega = 0 #0.02
     superconductor_params = {"w_0":w_0, "Delta":Delta,
               "mu":mu,
               "B_x":B_x, "B_y":B_y, "Lambda":Lambda,
               }
     
-    Gamma = 0.01
+    Gamma = 0.1
     alpha = 0
     beta = 0
     Beta = 1000
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     k_y_values = 2*np.pi*np.arange(0, L_x)/L_y
     # k_x_values = np.pi*np.arange(-L_x, L_x)/L_x
     # k_y_values = np.pi*np.arange(-L_y, L_x)/L_y
-    n_cores = 20
+    n_cores = 10
     # epsrel=1e-01
     
     # omega_values = np.linspace(-45, 0, 100)
@@ -68,7 +68,7 @@ if __name__ == "__main__":
         return [S.get_response_function_quad(0, 0, L_x, L_y, Gamma, fermi_function, Omega, part),
                 S.get_response_function_quad(1, 1, L_x, L_y, Gamma, fermi_function, Omega, part)]
     
-    B_values = np.linspace(0, 3*Delta, 20)
+    B_values = np.linspace(0, 3*Delta, 10)
     with multiprocessing.Pool(n_cores) as pool:
         results_pooled = pool.map(integrate, B_values)
     K = np.array(results_pooled)
--- plot_response_function_vs_B.py
+++ plot_response_function_vs_B.py
@@ -11,7 +11,7 @@ from pathlib import Path
 import numpy as np
 
 data_folder = Path("Data/")
-file_to_open = data_folder / "Response_kernel_vs_B_mu=-39_L=100_Gamma=0.01.npz"
+file_to_open = data_folder / "Response_kernel_vs_B_mu=-39_L=100_Gamma=0.01_Omega=0.001.npz"
 Data = np.load(file_to_open)
 
 K = Data["K"]
@@ -28,10 +28,10 @@ Gamma = Data["Gamma"]
 # L_y = Data["L_y"]
 
 fig, ax = plt.subplots()
-ax.plot(B_values/Delta, K[:, 0, 0], "-o",  label=r"$K^{(L)}_{xx}$")
-ax.plot(B_values/Delta, K[:, 1, 0], "-o",  label=r"$K^{(L)}_{yy}$")
-ax.plot(B_values/Delta, K[:, 0, 1], "-o",  label=r"$K^{(R)}_{xx}$")
-ax.plot(B_values/Delta, K[:, 1, 1], "-o",  label=r"$K^{(R)}_{yy}$")
+ax.plot(B_values/Delta, K[:, 0, 0], "-o",  label=r"$K^{(L)}_{xx}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 1, 0], "-o",  label=r"$K^{(L)}_{yy}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 0, 1], "-o",  label=r"$K^{(R)}_{xx}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 1, 1], "-o",  label=r"$K^{(R)}_{yy}(\Omega=$"+f"{Omega})")
 
 
 ax.set_title(r"$\lambda=$" + f"{Lambda:.2}"
@@ -45,4 +45,28 @@ ax.set_xlabel(r"$\frac{B_y}{\Delta}$")
 ax.set_ylabel(r"$K(B_y,\Omega=$"+f"{Omega})")
 ax.legend()
 plt.tight_layout()
-plt.legend()
+
+#%%
+file_to_open = data_folder / "Response_kernel_vs_B_mu=-39_L=100_Gamma=0.01.npz"
+Data = np.load(file_to_open)
+
+K = Data["K"]
+B_values = Data["B_values"]
+Lambda = Data["Lambda"]
+Delta = Data["Delta"]
+theta = Data["theta"]
+w_0 = Data["w_0"]
+mu = Data["mu"]
+part = Data["part"]
+Omega = Data["Omega"]
+Gamma = Data["Gamma"]
+# L_x = Data["L_x"]
+# L_y = Data["L_y"]
+
+ax.plot(B_values/Delta, K[:, 0, 0], "--o",  label=r"$K^{(L)}_{xx}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 1, 0], "--o",  label=r"$K^{(L)}_{yy}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 0, 1], "--o",  label=r"$K^{(R)}_{xx}(\Omega=$"+f"{Omega})")
+ax.plot(B_values/Delta, K[:, 1, 1], "--o",  label=r"$K^{(R)}_{yy}(\Omega=$"+f"{Omega})")
+
+ax.legend()
+plt.tight_layout()
